cc2c1c2b26bd93261d824ffec5b0ac284e00c717
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-01-08 01:56:19 +0900

    Scaffolding

---
+++ kisaragi-translate.el
@@ -0,0 +1,20 @@
+;;; kisaragi-translate.el --- A Lokalize equivalent -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024 Kisaragi Hiu
+;;
+;; Author: Kisaragi Hiu <mail@kisaragi-hiu.com>
+;; Version: 0.0.1
+;; Keywords: i18n apps
+;; Homepage: https://github.com/kisaragi-hiu/kisaragi-translate
+;; Package-Requires: ((emacs "29.1"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Might become a Lokalize equivalent in Emacs.
+;;
+;;; Code:
+
+(provide 'kisaragi-translate)
+;;; kisaragi-translate.el ends here
